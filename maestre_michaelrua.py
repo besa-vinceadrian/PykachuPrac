024bc3c8af0cce0c76f45b169bd31b082ea6a866
michaelrua <maestremichaelrua@gmail.com>
2025-03-12 15:20:09 +0800

    add "maestre_michaelrua.py"

---
+++ maestre_michaelrua.py
@@ -0,0 +1,11 @@
+Name = "Michael Rua S. Maestre"
+Birthday = "November 22, 2004"
+Gender = "Male"
+Email = "maestremichaelrua@gmail.com"
+
+print ("Name: " + Name)
+print ("Birthday: " + Birthday)
+print ("Gender: " + Gender)
+print ("Email: " + Email)
+
+
