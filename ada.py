dcfa769beab083cabd5831e786aab1b8acc40fd2
serquina-zcintilla <zcintillaserquina@gmaill.com>
2025-03-12 15:29:34 +0800

    asdawda

---
+++ ada.py
@@ -0,0 +1,9 @@
+Name = "Zcintilla R. Serquina"
+Birthday = "October 5, 2004"
+Gender = "Female"
+Email = "zcintillaserquina@gmail.com"
+
+print ("Name: " + Name)
+print ("Bithday: " + Birthday)
+print ("Gender: " + Gender)
+print ("Email: " + Email)
